728e2d901b326bff391ccb24581ec2644c684244
hamjakt21 <hamja.kutarvadliwala@infobeans.com>
2020-12-24 16:31:26 +0530

    Update Session Database to SQL DB

--- security.py
+++ security.py
@@ -1,19 +1,13 @@
-#In memory table
-from user import User
 from werkzeug.security import safe_str_cmp
-users = [
-	User(1, 'test', 'password')
-]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
+from user import User
 
 
 def authenticate(username, password):
-	user = username_mapping.get(username, None)
-	if user and safe_str_cmp(user.password, password):
-		return user
+    user = User.find_by_username(username)
+    if user and safe_str_cmp(user.password, password):
+        return user
+
 
 def identity(payload):
-	user_id = payload['identity']
-	return userid_mapping.get(user_id, None)
+    user_id = payload['identity']
+    return User.find_by_id(user_id)
